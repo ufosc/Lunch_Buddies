5725c4527de102cf824b974b11e682fd5e39992b
eileen-an1 <anyilin2002@gmail.com>
2022-11-17 21:19:16 -0500

    turned chat into a flatlist

--- client/src/Chat/Chat.js
+++ client/src/Chat/Chat.js
@@ -1,34 +1,55 @@
 import React from 'react'
-import { StyleSheet, Text, View, Image, SafeAreaView, Dimensions} from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView, Dimensions, FlatList, TouchableOpacity } from 'react-native';
 const win = Dimensions.get('window')
 
-function Message() {
-    return(
-        <View style={styles.Message}>
-            <Image source={require("../../assets/avatar.png")} style={styles.Picture}/>
-            <View style={styles.Preview}>
-                <Text style={styles.MessageName}>Albert Gator</Text>
-                <Text style={styles.MessageText}>This is a message preview</Text>
-            </View>
+const FakeData = [
+    {
+      id: '1',
+      messageName: 'Albert Gator',
+      messageText: 'This is a message preview blablablablab'
+    },
+    {
+      id: '2',
+      messageName: 'Alberta Gator',
+      messageText: 'This is a message preview'
+    },
+    {
+      id: '3',
+      messageName: 'IDK',
+      messageText: 'This is a message preview'
+    },
+  ];
+  
+  const Item = ({ messageName, messageText }) => (
+    <View style={styles.Message}>
+        <Image source={require("../../assets/avatar.png")} style={styles.Picture}/>
+        <View style={styles.Preview}>
+            <Text style={styles.MessageName}>{messageName}</Text>
+            <Text style={styles.MessageText}>{messageText}</Text>
         </View>
-    )
-}
-
-function Chat() {
-    return(
-        <SafeAreaView style={styles.Chat}>
-            <View style={styles.HeaderRow}>
+    </View>
+  );
+  
+  const Chat = () => {
+    const renderItem = ({ item }) => (
+      <Item messageName={item.messageName} 
+            messageText={item.messageText}/>
+    );
+  
+    return (
+      <SafeAreaView style={styles.Chat}>
+        <View style={styles.HeaderRow}>
                 {/*add hamburger menu and back arrow here*/}
                 <Text style={styles.Header}>Messages</Text>
-            </View>
-            <Message/>
-            <Message/>
-            <Message/>
-            <Message/>
-            <Message/>
-        </SafeAreaView>
-    )
-}
+        </View>
+        <FlatList
+          data={FakeData}
+          renderItem={renderItem}
+          keyExtractor={item => item.id}
+        />
+      </SafeAreaView>
+    );
+  }  
 
 const styles = StyleSheet.create({
     HeaderRow: {
@@ -51,12 +72,12 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
         borderRadius: 30,
         height: 90,
-        width: '80%',
+        width: '100%',
         marginTop: 13,
         alignItems: 'center',
-        shadowColor: 'black',
-        shadowOffset: {width: 5, height: -5},
-        shadowOpacity: 0.5,
+        //shadowColor: 'black',
+        //shadowOffset: {width: 5, height: -5},
+        //shadowOpacity: 0.5,
         elevation: 3
     },
     Picture: {
@@ -73,7 +94,7 @@ const styles = StyleSheet.create({
     },
     MessageName: {
         fontWeight: 'bold',
-        fontSize: 18
+        fontSize: 14
     },
     MessageText: {
         fontSize: 14,
